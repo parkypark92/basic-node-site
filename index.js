41fe7bbcace2c41296376a4edf695db73f268eeb
Andrew <115462877+parkypark92@users.noreply.github.com>
2024-04-16 12:34:40 +0100

    deal with favicon.ico and .css requests

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const http = require("node:http");
 const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./public/index.html", (err, data) => {
+  if (req.url === "/favicon.ico") {
+    res.writeHead(200, { "Content-Type": "image/x-icon" });
+    console.log("fackin favicon innit");
+    res.end();
+    return;
+  }
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url == "/" ? "index" : req.url
+  );
+  if (path.extname(req.url) === "") {
+    filePath += ".html";
+  }
+
+  let contentType = "text/html";
+  if (path.extname(filePath) === ".css") {
+    contentType = "text/css";
+  }
+
+  fs.readFile(filePath, (err, data) => {
     if (err) throw err;
+    res.writeHead(200, { "Content-Type": contentType });
+    console.log("request made");
     res.end(data);
   });
 });
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
