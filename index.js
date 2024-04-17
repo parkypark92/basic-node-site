2f29bd68f08c381859734426a81f896960f7deea
Andrew <115462877+parkypark92@users.noreply.github.com>
2024-04-17 08:52:44 +0100

    handle errors in server response

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const server = http.createServer((req, res) => {
   let filePath = path.join(
     __dirname,
     "public",
-    req.url == "/" ? "index" : req.url
+    req.url === "/"
+      ? "index.html"
+      : path.extname(req.url) === ""
+      ? req.url + ".html"
+      : req.url
   );
-  if (path.extname(req.url) === "") {
-    filePath += ".html";
-  }
 
   let contentType = "text/html";
   if (path.extname(filePath) === ".css") {
@@ -27,10 +28,18 @@ const server = http.createServer((req, res) => {
   }
 
   fs.readFile(filePath, (err, data) => {
-    if (err) throw err;
-    res.writeHead(200, { "Content-Type": contentType });
-    console.log("request made");
-    res.end(data);
+    if (err && err.code == "ENOENT") {
+      fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else if (err) {
+      res.writeHead(500);
+      res.end(`Server error. Error code: ${err.code}`);
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
   });
 });
 
